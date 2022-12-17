87541e4648f45f0c1bb8eeb90f7ac7e839c2be2c
AbinashG <abhi24964@gmail.com>
2022-12-17 19:56:48 +0530

    code-update

--- src/main/calculator.py
+++ src/main/calculator.py
@@ -19,5 +19,18 @@ class Calculator:
         return self._v1 / self._v2
 
 
-#if __name__ == "__main__":
-#    main()
+def main():
+    print("***** Prcoessing start *****")
+    value1, value2 = [int(x) for x in input('Enter two values for operation: ').split()]
+    print(f'First variable = {value1}, Second variable = {value2}')
+    cal_obj = Calculator(value1, value2)
+    print(f'''
+        Addition = {cal_obj.addition()},
+        Substraction = {cal_obj.substraction()},
+        Multiplication = {cal_obj.multiplication()},
+        Division = {cal_obj.divison()}
+        ''')
+
+
+if __name__ == "__main__":
+    main()
